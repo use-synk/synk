ee282425d429a36966a48c9f16342010620b6969
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-03-04 18:19:24 +0100

    feat(web): implement project runs page

--- apps/web/src/components/project/project-content.tsx
+++ apps/web/src/components/project/project-content.tsx
@@ -1,8 +1,24 @@
 import type { projectDetailSchema, runSummarySchema } from "@/api/endpoints";
+import { cn } from "@/lib/utils";
+import type { runStatusSchema } from "@synk-ai/shared";
 import { format } from "date-fns";
+import {
+	CheckIcon,
+	CircleXIcon,
+	ClockIcon,
+	GitBranchIcon,
+	GitCommitIcon,
+	Loader2Icon,
+	TriangleAlertIcon,
+	XIcon,
+} from "lucide-react";
+import Link from "next/link";
+import type React from "react";
+import { useMemo } from "react";
 import type z from "zod";
 import { type PageTabItem, PageTabList, PageTabPanel, PageTabs } from "../page-tabs";
 import { PageDescription, PageTitle } from "../typography";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { GithubLight } from "../ui/svgs/githubLight";
 
 export const projectTabItems: PageTabItem[] = [
@@ -92,14 +108,118 @@ function RunsTab({ runs }: { runs: z.infer<typeof runSummarySchema>[] }) {
 					</div>
 				</div>
 			</section>
-			<section className="grid grid-cols-1 gap-4">
-				{runs.length === 0 && <p>No runs found</p>}
-				{runs.map((run) => (
-					<div key={run.id} className="bg-stone-50 rounded-md p-4 w-full">
-						<pre>{JSON.stringify(run, null, 2)}</pre>
-					</div>
-				))}
+			<section className="mt-12">
+				<div className="w-full max-w-7xl mx-auto px-8">
+					<ul className="rounded-lg border border-stone-200 divide-y divide-stone-200">
+						{runs.map((run) => (
+							<RunRow key={run.id} run={run} />
+						))}
+					</ul>
+				</div>
 			</section>
 		</PageTabPanel>
+	);
+}
+
+function RunStatusIcon({
+	status,
+	className,
+	...props
+}: React.ComponentProps<"svg"> & { status: z.infer<typeof runStatusSchema> }) {
+	switch (status) {
+		case "queued":
+			return <ClockIcon className={cn("text-orange-500", className)} {...props} />;
+		case "running":
+			return <Loader2Icon className={cn("text-blue-500 animate-spin", className)} {...props} />;
+		case "completed":
+			return <CheckIcon className={cn("text-green-500", className)} {...props} />;
+		case "skipped":
+			return <CircleXIcon className={cn("text-gray-500", className)} {...props} />;
+		case "failed":
+			return <TriangleAlertIcon className={cn("text-red-500", className)} {...props} />;
+		case "canceled":
+			return <TriangleAlertIcon className={cn("text-gray-500", className)} {...props} />;
+		default:
+			return <XIcon className={cn("text-gray-500", className)} {...props} />;
+	}
+}
+
+function RunRow({ run }: { run: z.infer<typeof runSummarySchema> }) {
+	const title = useMemo(() => {
+		if (run.triggerType === "manual") {
+			return `Manual run on ${run.triggerRef}`;
+		}
+		if (run.triggerType === "push") {
+			return `Push to ${run.triggerRef}`;
+		}
+		if (run.triggerType === "merge" && run.prMessage) {
+			return (
+				<>
+					{run.prMessage}{" "}
+					<span className="text-stone-500">
+						(
+						<Link href={run.docPrUrl ?? ""} className="text-blue-500">
+							#{run.prNumber}
+						</Link>
+						)
+					</span>
+				</>
+			);
+		}
+		return "Unknown trigger type";
+	}, [run]);
+
+	return (
+		<li className="px-6 py-4">
+			<div className=" flex justify-start items-start gap-4">
+				<div className="flex justify-start items-center mt-1.25">
+					<RunStatusIcon status={run.status} className="size-4" />
+				</div>
+				<div>
+					<p className="font-medium">{title}</p>
+					<div className="mt-1 flex justify-start items-center gap-4">
+						{(run.prAuthorName || run.prAuthorUsername) && (
+							<div className="flex justify-start items-center gap-2 relative">
+								<Avatar className={"shrink-0 size-4"}>
+									<AvatarImage src={run.prAuthorAvatarUrl ?? ""} />
+									<AvatarFallback>
+										{run.prAuthorName?.charAt(0) ?? run.prAuthorUsername?.charAt(0) ?? "?"}
+									</AvatarFallback>
+								</Avatar>
+								<Link
+									href={`https://github.com/${run.prAuthorUsername}`}
+									className="text-sm text-stone-500 transition-colors hover:underline underline-offset-4"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<span className="absolute inset-0 w-full h-full" />
+									{run.prAuthorName ?? run.prAuthorUsername}
+								</Link>
+							</div>
+						)}
+
+						{/* {run.targetBranch && run.sourceBranch && (
+							<div className="flex justify-center items-center gap-1 text-sm text-stone-500">
+								<span>{run.sourceBranch}</span>
+								<ArrowRightIcon className="size-3.5 text-stone-400" />
+								<span>{run.targetBranch}</span>
+							</div>
+						)} */}
+						{run.triggerRef && (
+							<div className="flex justify-center items-center text-sm text-stone-500 gap-1.5">
+								<GitBranchIcon className="size-3.5 text-stone-400" />
+								{run.triggerRef}
+							</div>
+						)}
+						{run.triggerCommitSha && (
+							<div className="flex justify-center items-center text-sm text-stone-500 gap-1.5">
+								<GitCommitIcon className="size-3.5 text-stone-400" />
+								{run.triggerCommitSha.slice(0, 8)}
+							</div>
+						)}
+					</div>
+				</div>
+			</div>
+		</li>
 	);
 }
