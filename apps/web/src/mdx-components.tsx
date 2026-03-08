9269b44a1ce5bfec92a0703ef65cbeafc4f12933
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2026-03-09 00:48:49 +0100

    fix(web): import issue

--- apps/web/src/mdx-components.tsx
+++ apps/web/src/mdx-components.tsx
@@ -1,29 +1,28 @@
-import type { MDXComponents } from "mdx/types";
 import { cn } from "./lib/utils";
 
-const components: MDXComponents = {
-	p: ({ className, ...props }) => (
+const components = {
+	p: ({ className, ...props }: React.ComponentProps<"p">) => (
 		<p
 			className={cn("text-stone-500 text/relaxed mb-4 mt-6 first:mt-0 last:mb-0", className)}
 			{...props}
 		/>
 	),
-	ul: ({ className, ...props }) => (
+	ul: ({ className, ...props }: React.ComponentProps<"ul">) => (
 		<ul
 			className={cn("list-disc list-outside text-stone-500 text/relaxed mb-4 pl-6", className)}
 			{...props}
 		/>
 	),
-	ol: ({ className, ...props }) => (
+	ol: ({ className, ...props }: React.ComponentProps<"ol">) => (
 		<ol
 			className={cn("list-decimal list-outside text-stone-500 text/relaxed mb-4 pl-6", className)}
 			{...props}
 		/>
 	),
-	li: ({ className, ...props }) => (
+	li: ({ className, ...props }: React.ComponentProps<"li">) => (
 		<li
 			className={cn(
-				"relative text-stone-500 text/relaxed mb-1 pl-2 last:mb-0 marker:text-stone-300", // Change marker color here
+				"relative text-stone-500 text/relaxed mb-1 pl-2 last:mb-0 marker:text-stone-300",
 				className,
 			)}
 			{...props}
@@ -31,6 +30,6 @@ const components: MDXComponents = {
 	),
 };
 
-export function useMDXComponents(): MDXComponents {
+export function useMDXComponents() {
 	return components;
 }
